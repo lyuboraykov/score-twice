f2a2fca4111c3c3f075ebda8203eb074d55c4648
lyuboraykov <lyubo.raykov@gmail.com>
2013-10-24 12:13:39 +0300

    Menu tabs and search field

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,4 +5,22 @@ $(document).ready(function () {
     $("#scroll-container").niceScroll({
         touchbehavior: true
     });
+    //menu selection
+    $("#app-menu #sections-tab-menu li").click(selectMenuTab);
 });
+
+var selectMenuTab = function (e) {
+    var self = e.target;
+    $(self).css("background", "none");
+    $(self).css("border", "none");
+    $(self).css("outline", "none");
+
+    var tabs = $("#app-menu #sections-tab-menu li");
+    for (var i = 0; i < 3; i++) {
+        if ($(tabs[i]).attr('id') !== $(self).attr('id')) {
+            $(tabs[i]).css("background", "url('/images/non-selected-tab.fw.png')");
+            $(tabs[i]).css("border", "1px solid #272728");
+            $(tabs[i]).css("outline", "1px solid #3c3d3e");
+        }
+    }
+}
