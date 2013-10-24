1b794ddbb174d5f5f3ce3533a03ba1d4d9a9f30c
lyuboraykov <lyubo.raykov@gmail.com>
2013-10-24 14:36:17 +0300

    Menu list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,8 +5,12 @@ $(document).ready(function () {
     $("#scroll-container").niceScroll({
         touchbehavior: true
     });
+    $("#main-menu").niceScroll({
+        touchbehavior: true
+    });
     //menu selection
     $("#app-menu #sections-tab-menu li").click(selectMenuTab);
+    $("#app-menu #main-menu li").click(selectMenuItem);
 });
 
 var selectMenuTab = function (e) {
@@ -21,6 +25,18 @@ var selectMenuTab = function (e) {
             $(tabs[i]).css("background", "url('/images/non-selected-tab.fw.png')");
             $(tabs[i]).css("border", "1px solid #272728");
             $(tabs[i]).css("outline", "1px solid #3c3d3e");
+        }
+    }
+}
+
+var selectMenuItem = function (e) {
+    var self = e.target;
+    $(self).css("background", "black");
+    
+    var items = $("#app-menu #main-menu li");
+    for (var i = 0, length = items.length; i < length; i++) {
+        if ($(items[i]).attr('id') !== $(self).attr('id')) {
+            $(items[i]).css("background", "none");
         }
     }
 }
